46f0d327709a5f27200d3aa9df79deb41946afda
Muhammed Rashid N <85437745+rashidpkn@users.noreply.github.com>
2023-02-13 09:36:04 +0530

    404 termporary removed

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Route path='/' element={<><Navbar /><Outlet /><Footer /></>} >
         <Route index element={<Home />} />
       </Route>
-      <Route path='*' element={404} />
+      {/* <Route path='*' element={404} /> */}
     </Routes>
   )
 }

ef86eb593b5e67a64e9bf9d1ba6626e2d98a80a2
Muhammed Rashid N <85437745+rashidpkn@users.noreply.github.com>
2023-02-13 09:55:29 +0530

    GA is added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import { Outlet, Route, Routes } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Outlet, Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/home'
 import Footer from './shared/Footer'
 import Navbar from './shared/Navbar'
+import ReactGA from 'react-ga'
+ReactGA.initialize('G-9BDRRC9RLK')
 
 function App() {
+  const location = useLocation()
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search)
+  }, [location])
+  
   return (
     <Routes>
       <Route path='/' element={<><Navbar /><Outlet /><Footer /></>} >
